b13f1b868e0a0c9e3edfe87b33c5496b7e14f6c9
goutamp <pramanick.goutam@gmail.com>
2019-08-21 07:40:23 +0530

    Update CopyFile.ps1

--- CopyFile.ps1
+++ CopyFile.ps1
@@ -1,1 +1,1 @@
-Copy-Item -Path C:\temp\WebApp\bin\Debug\netcoreapp1.0 -Destination C:\inetpub\wwwroot -recurse -force
+Copy-Item -Path C:\temp\WebApp\bin\Debug\netcoreapp1.0\*.* -Destination C:\inetpub\wwwroot -recurse -force
